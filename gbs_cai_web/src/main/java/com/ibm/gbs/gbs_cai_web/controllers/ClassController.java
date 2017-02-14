59208f2c23ee35470e8f625271b7d9178703deec
0496sky@naver.com <kr700517@192.168.35.10>
2017-02-15 01:29:47 +0900

    Classes Controller 생성(초기틀 및 Commit 테스트)

--- gbs_cai_web/src/main/java/com/ibm/gbs/gbs_cai_web/controllers/ClassController.java
+++ gbs_cai_web/src/main/java/com/ibm/gbs/gbs_cai_web/controllers/ClassController.java
@@ -3,6 +3,9 @@ package com.ibm.gbs.gbs_cai_web.controllers;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +15,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ClassController {
+	protected final Log logger = LogFactory.getLog(getClass());
+	
 	@RequestMapping(value = {"/fetchClasses.do"}, method = RequestMethod.POST)
     public @ResponseBody List<Map<String, Object>> fetchClasses(@RequestParam("random")String random) throws Exception {
-		
+		logger.debug("First Commit Test");
 		List<Map<String, Object>> list = null;
 		return list;
     }
