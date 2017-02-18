a4c13a6c812b8730d80d1525f3692a5dea0fb79a
kr700512 <kr700512@kr700512>
2017-02-18 18:33:39 +0900

    AdminController 추가

---
+++ gbs_cai_web/src/main/java/com/ibm/gbs/gbs_cai_web/controllers/AdminController.java
@@ -0,0 +1,13 @@
+package com.ibm.gbs.gbs_cai_web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+
+	@RequestMapping("/adminClasslist")
+	public void displayClassList () {
+		
+	}
+}
