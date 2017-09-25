ff8024788f8660c71fd57c16d4ebe291fcf4b470
0496sky@naver.com <kr700517@172.30.57.209>
2017-09-25 20:03:35 +0900

    lombok, File Upload dependancy 추가

--- gbs_cai_web/src/main/java/com/ibm/gbs/gbs_cai_web/vo/AttendVO.java
+++ gbs_cai_web/src/main/java/com/ibm/gbs/gbs_cai_web/vo/AttendVO.java
@@ -1,5 +1,11 @@
 package com.ibm.gbs.gbs_cai_web.vo;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter @Setter
 public class AttendVO {
 	
 	private int idx;
@@ -7,36 +13,4 @@ public class AttendVO {
 	private String class_id;
 	private String attend_code;
 	
-	public int getIdx() {
-		return idx;
-	}
-	
-	public void setIdx(int idx) {
-		this.idx = idx;
-	}
-	
-	public String getUser_id() {
-		return user_id;
-	}
-	
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
-	
-	public String getClass_id() {
-		return class_id;
-	}
-	
-	public void setClass_id(String class_id) {
-		this.class_id = class_id;
-	}
-	
-	public String getAttend_code() {
-		return attend_code;
-	}
-	
-	public void setAttend_code(String attend_code) {
-		this.attend_code = attend_code;
-	}
-
 }
