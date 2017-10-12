745f03f0bcfe77fcca01fa7226a8652387bb8460
0496sky@naver.com <kr700517@192.168.35.10>
2017-10-13 00:12:20 +0900

    방/버스 정보 클릭시 페이지 안뜨는 현상 수정(일시적으로 페이지 redirect)

--- gbs_cai_web/src/main/java/com/ibm/gbs/gbs_cai_web/controllers/EnrollmentController.java
+++ gbs_cai_web/src/main/java/com/ibm/gbs/gbs_cai_web/controllers/EnrollmentController.java
@@ -4,6 +4,7 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,20 +19,20 @@ public class EnrollmentController {
 	private EnrollmentService enrollmentservice;
 
 	@RequestMapping("/myEnroll")
-	public ModelAndView myRoomInfo(@RequestParam("user_id") String user_id) throws Exception {
-		BasicConfigurator.configure();
-		ModelAndView mv = new ModelAndView();
+	public String myRoomInfo(Model model,@RequestParam("user_id") String user_id) throws Exception {
+		BasicConfigurator.configure();		
 		
-		logger.info("User ID : "+user_id);
 		try {
+			logger.info("USER ID : "+user_id);
 			EnrollmentVO enrollmentvo = enrollmentservice.getEnrollmentById(user_id);
-			mv.setViewName("");
-			mv.addObject("list", enrollmentvo);
+			logger.info("User ID : "+enrollmentvo.getUser_id()+" User Name : "+enrollmentvo.getUser_name()+" Depart Place : "+enrollmentvo.getDepart_place());
+			model.addAttribute("list", enrollmentvo);
+			
 		} catch (Exception e) {
 			logger.error("USER_ID is null in private enrollment info.", e);
 		}
 
-		return mv;
+		return "index";
 	}
 
 }
